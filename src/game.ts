82811fe951144268d430b1d4e1a2029a4aca1f55
Aleksandr Kolotovkin <aleksandrspoon@gmail.com>
2023-02-27 15:38:50 +0300

    change

--- src/game.ts
+++ src/game.ts
@@ -1,181 +1,263 @@
-const PAIRS = [3, 6, 9];
+import { emptyScreen } from './start-screen';
+import { templateEngine } from '../lib/template-engine.js';
+import { renderScreenLose } from './lose';
+import { renderScreenWin } from './win';
+import { cards } from './cards';
 
-function renderScreenGame() {
-  const app = emptyScreen();
+const PAIRS: number[] = [3, 6, 9];
+let startTimer: ReturnType<typeof setInterval>;
+let moves = 0; // кол-во угаданных пар карт
+let hasFlippedCard: boolean = false; // перевернутая карта
+let firstCard: HTMLElement, secondCard: HTMLElement;
+let timeCode: string;
 
-  window.application.blocks["newGame"] = renderNewGame;
-  window.application.blocks["cards"] = renderCards;
+export function renderScreenGame() {
+	emptyScreen();
+	const app = document.querySelector('.container') as HTMLElement;
 
-  app.appendChild(templateEngine(gameScreenTemplate()));
+	window.application.blocks['newGame'] = renderNewGame;
+	window.application.blocks['cards'] = renderCards;
 
-  const gameScreen = document.querySelector(".game__screen");
-  const headerScreen = document.querySelector(".game__screen-header");
+	app.appendChild(templateEngine(gameScreenTemplate()));
 
-  headerScreen.appendChild(templateEngine(playNewGameTemplate()));
+	const gameScreen = document.querySelector('.game__screen') as HTMLElement;
+	const headerScreen = document.querySelector(
+		'.game__screen-header'
+	) as HTMLElement;
 
-  window.application.renderBlock("newGame", headerScreen);
-  window.application.renderBlock("cards", gameScreen);
+	headerScreen.appendChild(templateEngine(playNewGameTemplate()));
+
+	window.application.renderBlock('newGame', headerScreen);
+	window.application.renderBlock('cards', gameScreen);
 }
 
 function renderNewGame() {
-  document.querySelector(".game__button").addEventListener("click", (event) => {
-    event.preventDefault();
+	const gameButton = document.querySelector('.game__button') as HTMLElement;
+	gameButton.addEventListener('click', (event) => {
+		event.preventDefault();
 
-    window.application.level = "";
-    window.application.renderScreen("gameLevel");
-  });
+		window.application.level = '';
+		window.application.renderScreen('gameLevel');
+	});
 }
 
 function playNewGameTemplate() {
-  return {
-    tag: "button",
-    cls: ["game__button", "button"],
-    text: "Начать заново",
-  };
+	return {
+		tag: 'button',
+		cls: ['game__button', 'button'],
+		text: 'Начать заново',
+	};
 }
 
 function timer() {
-  setTimeout(() => {
-    gameWatch();
-    coupCards();
-  }, 5000);
+	setTimeout(() => {
+		gameWatch();
+		coupCards();
+	}, 5000);
 }
 
 function gameWatch() {
-  const gameTimer = document.querySelector(".timer__degits");
+	const gameTimer = document.querySelector('.timer__degits') as HTMLElement;
 
-  let milliseconds = 0;
-  let startTimer;
+	let milliseconds = 0;
 
-  startTimer = setInterval(() => {
-    milliseconds += 1000;
+	startTimer = setInterval(() => {
+		milliseconds += 1000;
 
-    let dateTimer = new Date(milliseconds);
-
-    gameTimer.innerHTML =
-      dateTimer.getUTCMinutes() +
-      ":" +
-      ("0" + dateTimer.getUTCSeconds()).slice(-2);
-  }, 1000);
+		let dateTimer = new Date(milliseconds);
+		timeCode =
+			dateTimer.getUTCMinutes() +
+			':' +
+			('0' + dateTimer.getUTCSeconds()).slice(-2);
+		gameTimer.innerHTML = timeCode;
+	}, 1000);
 }
 
 function coupCards() {
-  const cardFront = document.querySelectorAll(".card__item-front");
-  const cardBack = document.querySelectorAll(".card__item-back");
+	const cardFront = document.querySelectorAll('.card__item-front');
+	const cardBack = document.querySelectorAll('.card__item-back');
 
-  cardBack.forEach((card) => {
-    card.classList.remove("card__item_hidden");
-  });
+	cardBack.forEach((card) => {
+		card.classList.remove('card__item_hidden');
+	});
 
-  cardFront.forEach((card) => {
-    card.classList.add("card__item_hidden");
-  });
+	cardFront.forEach((card) => {
+		card.classList.add('card__item_hidden');
+	});
 }
+
+type cardType = {
+	tag: string;
+	cls: string;
+	attrs: {
+		'data-id': string;
+	};
+	content: {
+		tag: string;
+		cls: string[];
+		attrs: {
+			width: string;
+			src: string;
+			'data-id'?: string;
+		};
+	}[];
+};
 
 function renderCards() {
-  timer();
+	timer();
 
-  // все карты
-  let allCardValues = cards;
-  // кол-во пар карт
-  const numberOfCards = PAIRS[window.application.level - 1];
-  // перемешивание карт
-  let cardValues2 = shuffleCards(allCardValues);
+	// все карты
+	let allCardValues: cardType[] = cards;
+	// кол-во пар карт
+	const numberOfCards = PAIRS[Number(window.application.level) - 1];
+	// перемешивание карт
+	let cardValues2 = shuffleCards(allCardValues);
 
-  cardValues2 = cardValues2.slice(0, numberOfCards);
-  cardValues2.push(...cardValues2);
-  cardValues2 = shuffleCards(cardValues2);
+	cardValues2 = cardValues2.slice(0, numberOfCards);
+	cardValues2.push(...cardValues2);
+	cardValues2 = shuffleCards(cardValues2);
 
-  // let cardValues2 = getRandomCards(numberOfCards, allCardValues);
-  // cardValues2.push(...cardValues2);
-  // cardValues2 = shuffleCards(cardValues2);
+	const cardsWrapper = document.querySelector('.cards__wrapper') as HTMLElement;
 
-  const cardsWrapper = document.querySelector(".cards__wrapper");
+	cardValues2.forEach((card: cardType) => {
+		cardsWrapper.appendChild(templateEngine(card));
+	});
 
-  cardValues2.forEach((card) => {
-    cardsWrapper.appendChild(templateEngine(card));
-  });
-
-  // let levelElems = document.querySelectorAll(".card__item");
-  // levelElems.forEach((item) => {
-  //   item.addEventListener("click", checkCard);
-  // });
+	let playCards: NodeListOf<Element> = document.querySelectorAll('.card__item');
+	playCards.forEach((playCard) => {
+		playCard.addEventListener('click', flipCard);
+	});
 }
 
-// function checkCard(e) {
-//   // открываем карту
-//   console.log(e.target);
-//   const childFront = e.target.firstChild;
-//   debugger;
-
-// }
-
-function shuffleCards(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let randomIndex = Math.floor(Math.random() * (i + 1));
-    [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
-  }
-  return array;
+function coupOneCard(children: HTMLCollection) {
+	// переворачиваем карту
+	for (let index = 0; index < children.length; index++) {
+		const element = children[index];
+		const childClassList = [...element.classList];
+		if (childClassList.includes('card__item-front')) {
+			element.classList.remove('card__item_hidden');
+		} else if (childClassList.includes('card__item-back')) {
+			element.classList.add('card__item_hidden');
+		}
+	}
 }
 
-function getRandomCards(pairsOfCards, cardsArray) {
-  let array = [];
-  for (let i = 0; i < pairsOfCards; i++) {
-    let randomIndex = Math.floor(Math.random() * (cardsArray.length - 1));
-    console.log("randomIndex ", randomIndex);
-    array[i] = cardsArray[randomIndex];
-  }
-  return array;
+function flipCard(this: HTMLElement) {
+	if (this === firstCard) return;
+
+	const children: HTMLCollection = this.children;
+	// переворачиваем одну карту
+	coupOneCard(children);
+
+	if (!hasFlippedCard) {
+		hasFlippedCard = true;
+		firstCard = this;
+		return;
+	}
+	console.log('firstCard', firstCard);
+	console.log('secondCard', secondCard);
+
+	secondCard = this;
+
+	checkCard();
+}
+
+function checkCard() {
+	if (firstCard.dataset.id === secondCard.dataset.id) {
+		disableCards();
+		return;
+	}
+
+	// остановка таймера!!!!
+	clearInterval(startTimer);
+	// сохранение времени
+	window.application.gameTime = timeCode;
+	console.log('вы проиграли');
+	// looseGame(); // Окошко вы проиграли
+	moves = 0;
+	// Экран проигрыша
+	window.application.screens['lose'] = renderScreenLose;
+	window.application.renderScreen('lose');
+}
+
+function disableCards() {
+	// для firstCard и secondCard убираем подписку на click через метод
+	firstCard.removeEventListener('click', flipCard);
+	secondCard.removeEventListener('click', flipCard);
+	console.log('disable');
+	//обнуляем карты
+	hasFlippedCard = false;
+	moves += 1;
+	if (moves === PAIRS[Number(window.application.level) - 1]) {
+		// остановка таймера!!!!
+		clearInterval(startTimer);
+		console.log('вы выйграли!!!');
+		// вы выйграли
+		moves = 0;
+		// сохранение времени
+		window.application.gameTime = timeCode;
+		// Экран выйгрыша
+		window.application.screens['win'] = renderScreenWin;
+		window.application.renderScreen('win');
+	}
+}
+
+function shuffleCards(array: cardType[]) {
+	for (let i = array.length - 1; i > 0; i--) {
+		let randomIndex = Math.floor(Math.random() * (i + 1));
+		[array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+	}
+	return array;
 }
 
 function gameScreenTemplate() {
-  return {
-    tag: "div",
-    cls: "game__screen",
-    content: [
-      {
-        tag: "div",
-        cls: "game__screen-header",
-        content: [
-          {
-            tag: "div",
-            cls: "game__screen-timer",
-            content: [
-              {
-                tag: "div",
-                cls: "timer__titles",
-                content: [
-                  {
-                    tag: "div",
-                    cls: "timer__title",
-                    text: "min",
-                  },
-                  {
-                    tag: "div",
-                    cls: "timer__title",
-                    text: "sek",
-                  },
-                ],
-              },
-              {
-                tag: "div",
-                cls: "timer__degits",
-                text: `00:00`,
-              },
-            ],
-          },
-        ],
-      },
-      {
-        tag: "section",
-        cls: "game__screen-cards",
-        content: [
-          {
-            tag: "div",
-            cls: "cards__wrapper",
-          },
-        ],
-      },
-    ],
-  };
+	return {
+		tag: 'div',
+		cls: 'game__screen',
+		content: [
+			{
+				tag: 'div',
+				cls: 'game__screen-header',
+				content: [
+					{
+						tag: 'div',
+						cls: 'game__screen-timer',
+						content: [
+							{
+								tag: 'div',
+								cls: 'timer__titles',
+								content: [
+									{
+										tag: 'div',
+										cls: 'timer__title',
+										text: 'min',
+									},
+									{
+										tag: 'div',
+										cls: 'timer__title',
+										text: 'sek',
+									},
+								],
+							},
+							{
+								tag: 'div',
+								cls: 'timer__degits',
+								text: `00:00`,
+							},
+						],
+					},
+				],
+			},
+			{
+				tag: 'section',
+				cls: 'game__screen-cards',
+				content: [
+					{
+						tag: 'div',
+						cls: 'cards__wrapper',
+					},
+				],
+			},
+		],
+	};
 }
--- src/start-screen.ts
+++ src/start-screen.ts
@@ -1,99 +1,128 @@
+import { renderScreenGame } from './game';
+import { templateEngine } from '../lib/template-engine.js';
 /* eslint-disable no-unused-vars */
-const LEVELS = ["easy_level", "medium_level", "hard_level"];
+const LEVELS = ['easy_level', 'medium_level', 'hard_level'];
 
-function emptyScreen() {
-  // очистка экрана
-  const app = document.querySelector(".container");
-  app.textContent = "";
-  return app;
+export function emptyScreen() {
+	// очистка экрана
+	const app = document.querySelector('.container') as HTMLElement;
+	app.textContent = '';
 }
 
-function renderBlockLevels(levelsBox) {
-  for (let index = 1; index <= 3; index++) {
-    const level = document.createElement("input");
-    level.classList.add("level-input");
-    level.type = "radio";
-    level.id = LEVELS[index - 1];
-    level.value = index;
+export function renderScreenGameLevel() {
+	emptyScreen();
+	const app = document.querySelector('.container') as HTMLElement;
 
-    const levelLabel = document.createElement("label");
-    levelLabel.textContent = index;
-    levelLabel.for = LEVELS[index - 1];
-    levelLabel.classList.add("level-label");
+	const section = document.createElement('section');
+	section.classList.add('screen', 'screen-level');
 
-    levelsBox.appendChild(level);
-    levelsBox.appendChild(levelLabel);
-  }
+	const form = document.createElement('form');
+	form.classList.add('form_level');
+
+	const header = document.createElement('h1');
+	header.textContent = 'Выбери сложность';
+	header.classList.add('level_header', 'element');
+
+	const divLevel = document.createElement('div');
+	divLevel.classList.add('element', 'elements__box');
+
+	const errorBlock = document.createElement('div');
+	errorBlock.classList.add('error__block', 'hidden__block');
+	const errorMessage = document.createElement('h3');
+	errorMessage.textContent = 'Пожалуйста, выберите уровень игры.';
+	errorBlock.appendChild(errorMessage);
+
+	const divLevelBox = document.createElement('div');
+	const buttonLevel = document.createElement('button');
+	buttonLevel.classList.add('btn_level');
+	buttonLevel.textContent = 'Старт';
+	divLevelBox.appendChild(buttonLevel);
+
+	divLevel.appendChild(errorBlock);
+	divLevel.appendChild(divLevelBox);
+
+	buttonLevel.addEventListener('click', (event) => {
+		event.preventDefault();
+		if (window.application.level) {
+			// загрузка экрана игры
+			window.application.screens['game'] = renderScreenGame;
+			renderScreenGame();
+		} else {
+			// вывод сообщения о том, что нужно выбрать уровень игры
+			errorBlock.classList.remove('hidden__block');
+		}
+	});
+
+	form.appendChild(header);
+	form.appendChild(templateEngine(levelScreenTemplate()));
+	form.appendChild(divLevel);
+
+	section.appendChild(form);
+
+	app.appendChild(section);
+
+	let levelElems = document.querySelectorAll('.level-label');
+	levelElems.forEach((item) => {
+		item.addEventListener('click', clickHandler);
+	});
+
+	function clickHandler(e: Event) {
+		// убираем выделение кнопки выбора уровня у кнопок
+		levelElems.forEach((item) => {
+			item.classList.remove('level_label--active');
+		});
+		if (e.target instanceof HTMLElement) {
+			e.target.classList.add('level_label--active');
+			window.application.level = e.target.textContent || '';
+			errorBlock.classList.add('hidden__block');
+		}
+	}
 }
 
-function renderScreenGameLevel() {
-  const app = emptyScreen();
-
-  const section = document.createElement("section");
-  section.classList.add("screen", "screen-level");
-
-  const form = document.createElement("form");
-  form.classList.add("form_level");
-
-  const header = document.createElement("h1");
-  header.textContent = "Выбери сложность";
-  header.classList.add("level_header", "element");
-
-  const levelsBox = document.createElement("div");
-  levelsBox.classList.add("level_select_div", "element");
-
-  renderBlockLevels(levelsBox);
-
-  const divLevel = document.createElement("div");
-  divLevel.classList.add("element", "elements__box");
-
-  const errorBlock = document.createElement("div");
-  errorBlock.classList.add("error__block", "hidden__block");
-  const errorMessage = document.createElement("h3");
-  errorMessage.textContent = "Пожалуйста, выберите уровень игры.";
-  errorBlock.appendChild(errorMessage);
-
-  const divLevelBox = document.createElement("div");
-  const buttonLevel = document.createElement("button");
-  buttonLevel.classList.add("btn_level");
-  buttonLevel.textContent = "Старт";
-  divLevelBox.appendChild(buttonLevel);
-
-  divLevel.appendChild(errorBlock);
-  divLevel.appendChild(divLevelBox);
-
-  buttonLevel.addEventListener("click", (event) => {
-    event.preventDefault();
-    if (window.application.level) {
-      // загрузка экрана игры
-      window.application.screens["game"] = renderScreenGame;
-      window.application.renderScreen("game");
-    } else {
-      // вывод сообщения о том, что нужно выбрать уровень игры
-      errorBlock.classList.remove("hidden__block");
-    }
-  });
-
-  form.appendChild(header);
-  form.appendChild(levelsBox);
-  form.appendChild(divLevel);
-
-  section.appendChild(form);
-
-  app.appendChild(section);
-
-  let levelElems = document.querySelectorAll(".level-label");
-  levelElems.forEach((item) => {
-    item.addEventListener("click", clickHandler);
-  });
-
-  function clickHandler(e) {
-    // убираем выделение кнопки выбора уровня у кнопок
-    levelElems.forEach((item) => {
-      item.classList.remove("level_label--active");
-    });
-    e.target.classList.add("level_label--active");
-    window.application.level = +e.target.textContent;
-    errorBlock.classList.add("hidden__block");
-  }
+function levelScreenTemplate() {
+	return {
+		tag: 'div',
+		cls: ['level_select_div', 'element'],
+		content: [
+			{
+				tag: 'input',
+				cls: 'level-input',
+				type: 'radio',
+				id: '1',
+				text: '1',
+			},
+			{
+				tag: 'label',
+				cls: 'level-label',
+				for: '1',
+				text: '1',
+			},
+			{
+				tag: 'input',
+				cls: 'level-input',
+				type: 'radio',
+				id: '2',
+				text: '2',
+			},
+			{
+				tag: 'label',
+				cls: 'level-label',
+				for: '2',
+				text: '2',
+			},
+			{
+				tag: 'input',
+				cls: 'level-input',
+				type: 'radio',
+				id: '3',
+				text: '3',
+			},
+			{
+				tag: 'label',
+				cls: 'level-label',
+				for: '3',
+				text: '3',
+			},
+		],
+	};
 }
