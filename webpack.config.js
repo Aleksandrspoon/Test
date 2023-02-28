aca5e0cfb0795fd1c21fe46db5c267f2f4d6bdaf
Aleksandr Kolotovkin <aleksandrspoon@gmail.com>
2023-03-01 01:13:46 +0300

    finish

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,53 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const mode =
+	process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
 module.exports = {
-  entry: "./src/index.ts",
-  mode: process.env.NODE_ENV === "production" ? "production" : "development",
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  optimization: {
-    minimizer: ["...", new CssMinimizerPlugin()],
-  },
-  devtool: process.env.NODE_ENV === "production" ? false : "source-map",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin()],
+	entry: './js/script.ts',
+	mode,
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: 'ts-loader',
+				exclude: /node_modules/, 
+			},
+			{ test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] }, 
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+			},
+		],
+	},
+	plugins: [
+		new CopyPlugin({
+			patterns: [{ from: 'static', to: 'static', noErrorOnMissing: true }],
+		}),
+		new HtmlWebpackPlugin({
+			template: './index.html',
+		}),
+		new MiniCssExtractPlugin({}),
+	],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		clean: true,
+	},
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+	optimization: {
+		minimizer: ['...', new CssMinimizerPlugin()],
+	},
+	devtool:
+		process.env.NODE_ENV === 'production' ? 'hidden-source-map' : 'source-map',
 };
